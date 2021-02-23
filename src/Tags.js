e34e577e7652fb88f30af947ad2a6428cec435fe
Karin Liegmann <karin.liegmann@googlemail.com>
2021-02-23 21:36:15 +0100

    hid tag-container in div instead of input field

--- src/Tags.js
+++ src/Tags.js
@@ -16,40 +16,72 @@ export default function Tags({ createTag, onDeleteTag, tags }) {
         }
     }
 
-
-
-
     return (
-        <section>
+        <TagWrapper>
             <label>Product Tags: </label>
-            <input
-                type="text"
-                name="product_tags"
-                value={value}
-                onChange={handleChange}
-                onKeyDown={handleKeyDown} />
-
-            <TagList>
+            <TagDiv>
                 {tags.map((tag, index) =>
                     (<span key={index}>{tag}<i onClick={() => onDeleteTag(tag)}>&times;</i></span>)
                 )}
-            </TagList>
+                <input
+                    type="text"
+                    name="product_tags"
+                    value={value}
+                    onChange={handleChange}
+                    onKeyDown={handleKeyDown}
+                    size="5" />
+            </TagDiv>
 
-
-        </section>
+        </TagWrapper>
     )
 }
 
-const TagList = styled.section`
+const TagWrapper = styled.section`
 display: flex;
-flex-wrap: wrap;
-margin-top: 1rem;
+flex-wrap: none;
+margin: .6rem 0;
+padding: 0.1rem .2rem;
+width: 330px;
 
-span {
-    background: linear-gradient(45deg, #65CDD2, #C4DAC6);
-    border-radius: 4px;
-    font-weight: 600;
-    margin-left: .5rem;
-    padding: .3rem 1rem;
-}
+    label {
+        white-space: nowrap;
+    }
+`
+
+
+const TagDiv = styled.div`
+    background: white;
+    border: 1px solid grey;
+    border-radius: 6px;
+    display: flex;
+    flex-wrap: wrap;
+    height: fit-content;
+    margin-left: .3rem;
+    width: 100%;
+    z-index: 10;
+
+    input {
+        border: none; 
+        display: inline;
+        margin-left: 5px;       
+        }
+
+        input:focus {
+        outline: none;     
+        }
+
+    span {
+        background: linear-gradient(45deg, #65CDD2, #C4DAC6);
+        border-radius: 4px;
+        margin: .1rem 0;
+        margin-left: .3rem;
+        padding: .1rem .3rem;
+        z-index: 20;
+    }
+
+    i {
+        color: red;
+        font-weight: bold;
+        margin-left: .5rem;
+    }
 `
